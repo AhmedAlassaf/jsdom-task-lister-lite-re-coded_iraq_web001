9344a446bf2c254a26e58ae5001d313209d11fd7
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-22 22:23:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,17 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     let tasks = document.getElementById("tasks");
     let description = getElementById('new-task-description').value;
-    if (description == "") {
-        alert("Add a ToDo!");
-    }
 
-    else {
         
         let item = document.createElement('li');
         item.innerHTML = `<div>${description}</div>`;
         tasks.appendChild(item);
 
         description = document.getElementById('new-task-description').value ='';
-    }
   });
 });
