cf571cf80121a95db4df56450f732dc6280c10a3
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-22 22:16:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
   let submit = document.getElementsByTagName('input')[1];
   submit.addEventListener('click', function(e){
     event.preventDefault();
+    let tasks = document.getElementById("tasks").value;
+    let addedToDos = document.getElementById("addedToDos");
+    if (text == "", deadline == "") {
+        alert("Add a ToDo!");
+    }
+
+    else {
+        
+        let newToDo = document.createElement('li');
+        newToDo.innerHTML = `<div>${text}</div><div>${deadline}</div>`;
+        addedToDos.appendChild(newToDo);
+
+        text = document.getElementById('text').value ='';
+        deadline = document.getElementById('deadline').value ='';
+    }
   });
   
   let tasksList=document.getElementById('tasks');
