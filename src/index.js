02e5fe47fa400d3c46971e19f84b92dabec63ddd
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-22 22:15:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submit = document.getElementsByTagName('input')[1];
+  submit.addEventListener('click', function(e){
+    event.preventDefault();
+  });
+  
+  let tasksList=document.getElementById('tasks');
+  let listItem=document.createElement('li');
+  let delBtn=document.createElement('button');
+  listItem.appendChild(delBtn);
+  tasksList.appendChild(listItem);
 });
