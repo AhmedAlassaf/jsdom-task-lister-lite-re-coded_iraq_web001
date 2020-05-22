a9b3c6d44513e7d1b935b731ffd27e5c728480bc
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-22 22:26:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submit = document.getElementsByTagName('input')[1];
   submit.addEventListener('click', function(e){
-    event.preventDefault();
+    
     let tasks = document.getElementById("tasks");
     let description = getElementById('new-task-description').value;
-
-        
-        let item = document.createElement('li');
-        item.innerHTML = `<div>${description}</div>`;
-        tasks.appendChild(item);
-        description = document.getElementById('new-task-description').value ='';
+    let item = document.createElement('li');
+    item.innerHTML = `<div>${description}</div>`;
+    event.preventDefault();   
   });
 });
